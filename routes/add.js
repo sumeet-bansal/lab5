a3577440077554feec771ddf818b5108c95a8a0a
Sumeet Bansal <sumeetbansal@gmail.com>
2018-02-07 18:19:31 -0800

    finished lab 5

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,12 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+exports.addFriend = function(req, res) { 
+	var name = req.query.name;
+	var desc = req.query.description;
+	var newFr = {
+		"name" : name,
+		"description" : desc,
+		"imageURL" : "https://lorempixel.com/400/400.people"
+	}
+	data.friends.push(newFr);
+}
